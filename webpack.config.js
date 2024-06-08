131fe79a292812677bb7554cdc1628d13103adea
Rodrigo Sousa <rodrigoasferreira@protonmail.com>
2024-06-08 16:10:13 -0300

    webpack with webpack-dev-server
    great for dev, it automagically recompiles you code as you save

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,26 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: {
     index: "./src/index.js",
     print: "./src/print.js",
   },
+  devtool: "inline-source-map", // great for bug tracking but only in development mode!!!
+  devServer: {
+    static: "./dist",
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Output Management",
+      title: "Development",
     }),
   ],
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+  },
+  optimization: {
+    runtimeChunk: "single",
   },
 };
